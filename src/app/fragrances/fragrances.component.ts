b79bff5cdf03379f5d4f11e7d7b9b62ab5155883
petrovska4 <tijanapetrovska173@gmail.com>
2024-02-08 01:24:17 +0100

    day2

--- src/app/fragrances/fragrances.component.ts
+++ src/app/fragrances/fragrances.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FRAGRANCES } from '../data-base/fragrancesDB';
+import { ProductComponent } from '../product/product.component';
 
 @Component({
   selector: 'app-fragrances',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './fragrances.component.html',
   styleUrl: './fragrances.component.css'
 })
 export class FragrancesComponent {
+
+  fragrances = FRAGRANCES;
 
 }
